7ae4f655a40fa9244357066a278f46cdb4acfb25
zhangxidong <zxdong_55@163.com>
2022-08-04 22:07:06 +0800

    add ch02 bytecode start

--- repl/repl.go
+++ repl/repl.go
@@ -3,10 +3,10 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"go-interpreter/evaluator"
+	"go-interpreter/compiler"
 	"go-interpreter/lexer"
-	"go-interpreter/object"
 	"go-interpreter/parser"
+	"go-interpreter/vm"
 	"io"
 )
 
@@ -14,7 +14,6 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -33,11 +32,23 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
